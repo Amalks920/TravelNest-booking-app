3f67268f307a0097573570a9b0c4b013488cc2a4
amalks920 <amalks920@gmail.com>
2024-03-07 09:48:18 +0530

    done

--- server/src/config/cors/allowedOrigins.js
+++ server/src/config/cors/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:4000",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://travelnest.shop"
 ];
 
 module.exports = allowedOrigins;
